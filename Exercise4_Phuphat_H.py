0a9a28abb1f2186bedbada10a986c3d6e5e5b755
Dai-Phu <78296473+Dai-Phu@users.noreply.github.com>
2021-02-05 23:22:42 +0700

    Create Exercise4_Phuphat_H.py

---
+++ Exercise4_Phuphat_H.py
@@ -0,0 +1,9 @@
+Fe = 88.88
+Gb = 89.55
+Ics = 78.28
+Cp = 96.29
+print("--------------Your Score--------------")
+print("Foundation English               =", Fe) 
+print("General Business                 =", Gb)
+print("Introduction to Computer Systems =", Ics)
+print("Computer Programming             =", Cp)
