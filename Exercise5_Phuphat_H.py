75d85be37f7605b1a0838aa6f2c1fad306df58ff
Dai-Phu <78296473+Dai-Phu@users.noreply.github.com>
2021-03-07 14:04:41 +0700

    Create Exercise5_Phuphat_H.py

---
+++ Exercise5_Phuphat_H.py
@@ -0,0 +1,6 @@
+num1 = int(input("number1 :"))
+num2 = int(input("number2 :"))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
